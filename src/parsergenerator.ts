b7cfd497441d826f2bc33c6c3211dc1dd3fd1fd4
Tatamo <nsorder@gmail.com>
2016-11-21 11:51:42 +0900

    discriminate terminal symbol or not

--- src/parsergenerator.ts
+++ src/parsergenerator.ts
@@ -5,7 +5,7 @@ module ParserGenerator{
 		ltoken: string;
 		pattern: Array<Array<string>>;
 	}
-	export interface SyntaxDefinitions extends Array<SyntaxDefinitionSection>{}
+	export type SyntaxDefinitions = Array<SyntaxDefinitionSection>;
 	export var def:SyntaxDefinitions = [
 		{
 			ltoken: "EXP",
@@ -17,6 +17,38 @@ module ParserGenerator{
 		}
 	];
 	export class TerminalSymbolDiscriminator{
+		token_table: Array<{type:string, is_terminal:boolean}>;
+		constructor(lexdef:Lexer.LexDefinitions, syntaxdef:SyntaxDefinitions){
+			this.token_table = [];
+			for(var i=0; i<lexdef.length; i++){
+				if(lexdef[i].type == null){
+					continue;
+				}
+				var flg_push = true;
+				for(var ii=0; ii<this.token_table.length; ii++){
+					if(this.token_table[ii].type == lexdef[i].type){
+						flg_push = false;
+						break;
+					}
+				}
+				if(flg_push){
+					this.token_table.push({type : lexdef[i].type, is_terminal : true});
+				}
+			}
+			for(var i=0; i<syntaxdef.length; i++){
+				var flg_token_not_found = true;
+				for(var ii=0; ii<this.token_table.length; ii++){
+					if(syntaxdef[i].ltoken == this.token_table[ii].type){
+						this.token_table[ii].is_terminal = false;
+						flg_token_not_found = false;
+						break;
+					}
+				}
+				if(flg_token_not_found){
+					throw new Error("token not found in lexical definitions");
+				}
+			}
+		}
 	}
 	export class ParserGenerator{
 	}
