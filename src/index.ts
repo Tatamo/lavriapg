4e8c06fdb7a80dff39df93fc5a886d3a38fb519b
Tatamo <nsorder@gmail.com>
2017-01-03 17:44:08 +0900

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from "./ast";
+export * from "./factory";
+export * from "./grammar";
+export * from "./lexer";
+export * from "./parser";
+export * from "./parsergenerator";
+export * from "./parsingtable";
+export * from "./precompiler";
+export * from "./ruleparser";
+export * from "./symboldiscriminator";
+export * from "./token";
+
