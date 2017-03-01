dbcb0f74a6a29a4eafb863a2a71e52cedaa831d7
Tatamo <nsorder@gmail.com>
2017-03-01 13:44:31 +0900

    refactoring NullableSet (and fix bug)

--- src/parsergenerator/nullableset.ts
+++ src/parsergenerator/nullableset.ts
@@ -11,37 +11,33 @@ export class NullableSet{
 		// 制約条件を導出するために、
 		// 空列になりうる記号の集合nullsを導出
 		this.nulls = new Set<Token>();
-		for(let i=0; i<this.grammar.syntax.length; i++){
-			let ltoken = this.grammar.syntax[i].ltoken;
-			let pattern = this.grammar.syntax[i].pattern;
-
+		for(let rule of this.grammar.syntax){
 			// 右辺の記号の数が0の規則を持つ記号は空列になりうる
-			if(pattern == []){
-				this.nulls.add(ltoken);
+			if(rule.pattern.length == 0){
+				this.nulls.add(rule.ltoken);
 			}
 		}
-		let flg_changed:boolean = true;
+
 		// 変更が起きなくなるまでループする
+		let flg_changed:boolean = true;
 		while(flg_changed){
 			flg_changed = false;
-			for(let i=0; i<this.grammar.syntax.length; i++){
-				let ltoken = this.grammar.syntax[i].ltoken;
-
+			for(let rule of this.grammar.syntax){
 				// 既にnullsに含まれていればスキップ
-				if(this.isNullable(ltoken)) continue;
+				if(this.isNullable(rule.ltoken)) continue;
 
-				let pattern = this.grammar.syntax[i].pattern;
 				let flg_nulls = true;
 				// 右辺に含まれる記号がすべてnullableの場合はその左辺はnullable
-				for(let ii=0; ii<pattern.length; ii++){
-					if(!this.isNullable(pattern[ii])){
+				for(let token of rule.pattern){
+					if(!this.isNullable(token)){
+						// 一つでもnullableでない記号があるならnon-nullable
 						flg_nulls = false;
 						break;
 					}
 				}
 				if(flg_nulls){
-					if(this.nulls.has(ltoken)) flg_changed = true;
-					else this.nulls.add(ltoken);
+					flg_changed = true;
+					this.nulls.add(rule.ltoken);
 				}
 			}
 		}
