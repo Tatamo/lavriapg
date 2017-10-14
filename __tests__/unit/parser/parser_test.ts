df37e4628adeda821e3dda14218e1d39ddf636c5
Tatamo <nsorder@gmail.com>
2017-10-15 03:23:56 +0900

    add parser test

---
+++ __tests__/unit/parser/parser_test.ts
@@ -0,0 +1,28 @@
+import {Parser} from "../../../src/parser/parser";
+import {ParserFactory} from "../../../src/parser/factory";
+import {ParserGenerator} from "../../../src/parsergenerator/parsergenerator";
+import {test_calc_grammar, test_calc_solver} from "../../data/sample_grammar";
+
+describe("parser test", () => {
+	const parsingtable = new ParserGenerator(test_calc_grammar).getParsingTable();
+	const parser = ParserFactory.create(test_calc_grammar, new ParserGenerator(test_calc_grammar).getParsingTable());
+	test("parser factory", () => {
+		expect(ParserFactory.create(test_calc_grammar, parsingtable)).toBeInstanceOf(Parser);
+	});
+	test("getting calc grammar ast", () => {
+		expect(parser.parse("1+1")).toEqual({
+			type: "EXP", value: null, children:
+				[
+					{type: "EXP", value: null, children: [{type: "TERM", value: null, children: [{type: "ATOM", value: null, children: [{type: "DIGITS", value: "1", children: []}]}]}]},
+					{type: "PLUS", value: "+", children: []},
+					{type: "TERM", value: null, children: [{type: "ATOM", value: null, children: [{type: "DIGITS", value: "1", children: []}]}]}
+				]
+		});
+	});
+	test("invalid input", () => {
+		expect(parser.parse("1zzz")).toEqual({type: "DIGITS", value: "1", children: []});
+	});
+	test("custom callback", () => {
+		expect(parser.parse("2*(3+4)", test_calc_solver)).toBe(14);
+	});
+});
