e4cbf5e6272d23c47e704f3632f565bc2a6381de
Tatamo <nsorder@gmail.com>
2017-11-07 03:42:01 +0900

    define a custom callback controller to pass the failed test

--- __tests__/precompiler/precompiler_test.ts
+++ __tests__/precompiler/precompiler_test.ts
@@ -1,6 +1,45 @@
 import {PreCompiler} from "../../src/precompiler/precompiler";
 import {test_calc_language_raw_string} from "../data/sample_language";
+import {AbstractCallbackController} from "../../src/parser/callback";
+import {ILexer} from "../../dist/lexer/lexer";
 import * as fs from "fs";
+
+class CustomCallbackController extends AbstractCallbackController {
+	callLex(id: number, value: any, lexer: ILexer) {
+		const rule = this.language.lex[id];
+		if (rule.token == "DIGITS") {
+			return +value;
+		}
+		return null;
+	}
+	callGrammar(id: number, children: Array<any>, lexer: ILexer) {
+		const rule = this.language.grammar[id];
+		if (rule.ltoken == "ATOM") {
+			if (children.length == 1) {
+				return children[0];
+			}
+			else {
+				return children[1];
+			}
+		}
+		else if (rule.ltoken == "TERM") {
+			if (children.length == 1) {
+				return children[0];
+			}
+			else {
+				return children[0] * children[2];
+			}
+		}
+		else if (rule.ltoken == "EXP") {
+			if (children.length == 1) {
+				return children[0];
+			}
+			else {
+				return children[0] + children[2];
+			}
+		}
+	}
+}
 
 describe("precompiler test", () => {
 	const precompiler = new PreCompiler("../../../dist");
@@ -10,8 +49,8 @@ describe("precompiler test", () => {
 	test("parse \"1+1\" by using compiled parser", () => {
 		expect(() => p.parser.parse("1+1")).not.toThrow();
 	});
-	// TODO: 外部からcallbackを与えられるようにする
-	test("parse \"1+1\" equals to 2 by using copmpiled parser", () => {
+	test("parse \"1+1\" equals to 2 by using compiled parser and custom callback controller", () => {
+		p.parser.setCallbackController(new CustomCallbackController(p.language));
 		expect(p.parser.parse("1+1")).toBe(2);
 	});
 	fs.unlinkSync("./__tests__/data/tmp/precompiler_result.ts");
