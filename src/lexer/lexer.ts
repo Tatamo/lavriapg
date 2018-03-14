936caea8060e3868d88161bb92feee90c7628a70
Tatamo <nsorder@gmail.com>
2018-03-14 22:21:11 +0900

    add LexRuleManager

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -10,51 +10,136 @@ export interface ILexer {
 	exec(input: string): Array<TokenizedInput>;
 }
 
-export class LexController {
-	private _lex: LexDefinition;
-	private _current_state: LexStateLabel;
-	private _state_stack: Array<LexStateLabel>;
-	private _temporary_rules: { states: Map<LexStateLabel, Set<string>>, rules: Map<string, LexRule> };
-	private _rules: Map<LexStateLabel, Array<LexRule>>;
-	private _states: Map<LexStateLabel, LexState>;
+export type LexRuleLabel = string;
+
+class LexRuleManager {
+	// private states: { states: Map<LexStateLabel, LexState>, index: Map<LexStateLabel, Set<number>>, inheritance: Map<LexStateLabel, LexStateLabel> };
+	private states: Map<LexStateLabel, { state: LexState, index: Set<number>, inheritance: LexStateLabel | null }>;
+	private rules: { rules: Array<LexRule | undefined>, labels: Map<LexRuleLabel, number> };
+	// 各ルールに一意なidを割り当てるためのカウンタ
+	private id_counter: number;
+	// ルールの削除によって割り当てがなくなったid
+	private free_ids: Array<number>;
 	constructor(language: Language) {
-		this._lex = language.lex;
-		this._current_state = default_lex_state;
-		this._state_stack = [];
-		this._temporary_rules = {states: new Map(), rules: new Map()};
+		const lex = language.lex;
+		this.id_counter = 0;
+		this.free_ids = [];
 
 		// initialize lex states map
-		this._states = new Map();
+		this.states = new Map();
 		// もしlexの定義内にデフォルト状態の記述があっても上書きされるだけなので問題ない
-		this._states.set(default_lex_state, {label: default_lex_state, is_exclusive: false});
-		if (this._lex.states !== undefined) {
-			for (const state of this._lex.states) {
-				this._states.set(state.label, LexController.formatLexState(state));
+		this.addState({label: default_lex_state, is_exclusive: false});
+		if (lex.states !== undefined) {
+			for (const state of lex.states) {
+				this.addState(state);
 			}
 		}
 
 		// initialize lex rules
-		this._rules = new Map();
-		this._rules.set(default_lex_state, []);
-		for (const _rule of this._lex.rules) {
-			// clone rule
-			const rule = LexController.formatLexRule(_rule);
-			// 状態ごとに登録
+		this.rules = {rules: [], labels: new Map()};
+
+		for (const rule of lex.rules.map((r) => LexRuleManager.formatLexRule(r))) {
+			this.rules.rules[this.id_counter] = rule;
+			// 状態ごとにインデックスを張る
 			for (const state of rule.state!) {
-				if (!this._rules.has(state)) {
-					this._rules.set(state, []);
+				// TODO: statesに入れる
+				if (!this.states.has(state)) {
+					this.addState(state);
 				}
-				this._rules.get(state)!.push(rule);
+				this.states.get(state)!.index.add(this.id_counter);
 			}
+			this.id_counter += 1;
 		}
 	}
-	private static formatLexState(state: LexState): LexState {
+	addState(label: LexStateLabel): boolean;
+	addState(state: LexState): boolean;
+	addState(s: LexStateLabel | LexState): boolean {
+		let state: LexState;
+		if (typeof s === "object") {
+			state = s;
+		}
+		else {
+			state = {label: s};
+		}
+		state = LexRuleManager.formatLexState(state);
+		if (this.states.has(state.label)) {
+			return false;
+		}
+		const inheritance: LexStateLabel | null = state.is_exclusive ? null : default_lex_state;
+		this.states.set(state.label, {state, index: new Set(), inheritance});
+		// ループチェック
+		if (inheritance !== null) {
+			let flg_loop = false;
+			let parent = this.states.get(inheritance);
+			while (parent !== undefined && parent.inheritance !== null) {
+				// 状態を追加するたびにチェックするので、自身にたどりつかないことを調べればよい
+				if (parent.inheritance === state.label) {
+					flg_loop = true;
+					break;
+				}
+				parent = this.states.get(parent.inheritance);
+			}
+			if (flg_loop) this.states.get(state.label)!.inheritance = null;
+		}
+		return true;
+	}
+	getRulesItr(state: LexStateLabel): IterableIterator<LexRule> {
+		// そんな状態はない
+		if (!this.states.has(state)) return [][Symbol.iterator]();
+
+		// 継承を加味
+		let result: Array<number> = [];
+		let s = this.states.get(state);
+		while (s !== undefined) {
+			result = result.concat([...s.index]);
+			if (s.inheritance === null) break;
+			s = this.states.get(s.inheritance);
+		}
+
+		return (function* (self, itr) {
+			for (const id of itr) {
+				if (self.rules.rules[id] !== undefined) yield self.rules.rules[id]!;
+			}
+		})(this, new Set(result)[Symbol.iterator]());
+	}
+	addRule(label: LexRuleLabel, rule: LexRule): void {
+		// 同名の既存ルールを破棄
+		this.removeRule(label);
+
+		const formatted_rule = LexRuleManager.formatLexRule(rule);
+
+		const id = this.free_ids.length > 0 ? this.free_ids.pop()! : this.id_counter++;
+		this.rules.rules[id] = formatted_rule;
+		this.rules.labels.set(label, id);
+		for (const state of formatted_rule.state!) {
+			if (!this.states.has(state)) this.addState(state);
+			this.states.get(state)!.index.add(id);
+		}
+	}
+	removeRule(label: LexRuleLabel): LexRule | null {
+		if (!this.rules.labels.has(label)) {
+			return null;
+		}
+		const id = this.rules.labels.get(label)!;
+		const rule = this.rules.rules[id];
+		if (rule === undefined) return null;
+
+		for (const state of rule.state!) {
+			if (this.states.has(state)) {
+				this.states.get(state)!.index.delete(id);
+			}
+		}
+		this.rules.rules[id] = undefined;
+		this.free_ids.push(id);
+		return rule;
+	}
+	static formatLexState(state: LexState): LexState {
 		return {
 			label: state.label,
 			is_exclusive: state.is_exclusive !== undefined ? state.is_exclusive : false
 		};
 	}
-	private static formatLexRule(rule: LexRule): LexRule {
+	static formatLexRule(rule: LexRule): LexRule {
 		// clone rule
 		const result = {...rule};
 		if (result.is_disabled === undefined) result.is_disabled = false;
@@ -62,7 +147,7 @@ export class LexController {
 		if (result.state === undefined) result.state = [default_lex_state];
 		// 正規表現を字句解析に適した形に整形
 		if (result.pattern instanceof RegExp) {
-			result.pattern = LexController.formatRegExp(result.pattern);
+			result.pattern = LexRuleManager.formatRegExp(result.pattern);
 		}
 		return result;
 	}
@@ -84,97 +169,27 @@ export class LexController {
 		flags += "y";
 		return new RegExp(pattern, flags);
 	}
-	/**
-	 * 字句解析器の状態を与えると、それに対応する動的に追加されたルールを返す
-	 * @param {LexStateLabel} state 字句解析機の状態を表すラベル
-	 * @returns {Array<LexRule>} 対応する字句ルール
-	 */
-	private getTemporaryRules(state: LexStateLabel): Array<LexRule> {
-		const result: Array<LexRule> = [];
-		const set: Set<string> = this._temporary_rules.states.has(state) ? this._temporary_rules.states.get(state)! : new Set();
-		for (const label of set) {
-			if (this._temporary_rules.rules.has(label)) {
-				result.push(this._temporary_rules.rules.get(label)!);
-			}
-		}
-		if (state !== default_lex_state && this._states.has(state) && !this._states.get(state)!.is_exclusive) {
-			if (this._temporary_rules.states.has(default_lex_state)) {
-				for (const rule of this.getTemporaryRules(default_lex_state)) {
-					result.push(rule);
-				}
-			}
-		}
-		return result;
-	}
-	private getBasicRules(state: LexStateLabel): Array<LexRule> {
-		let result: Array<LexRule>;
-		if (this._rules.has(state)) {
-			result = this._rules.get(state)!;
-		}
-		else {
-			result = [];
-		}
-		if (this._states.has(state) && !this._states.get(state)!.is_exclusive) {
-			result = result.concat(this._rules.get(default_lex_state)!);
-		}
-		return result;
+}
+
+export class LexController {
+	private _lex: LexDefinition;
+	private _current_state: LexStateLabel;
+	private _state_stack: Array<LexStateLabel>;
+	private _rules: LexRuleManager;
+	constructor(language: Language) {
+		this._lex = language.lex;
+		this._current_state = default_lex_state;
+		this._state_stack = [];
+		this._rules = new LexRuleManager(language);
 	}
 	getRulesItr(): IterableIterator<LexRule> {
-		const basic_r: Array<LexRule> = this.getBasicRules(this._current_state);
-		const tmp_r: Array<LexRule> = this.getTemporaryRules(this._current_state);
-		const itr_basic_rules = basic_r[Symbol.iterator]();
-		const itr_temporary_rules = tmp_r[Symbol.iterator]();
-		const itr: IterableIterator<LexRule> = {
-			next: (): IteratorResult<LexRule> => {
-				let next;
-				while (true) {
-					next = itr_basic_rules.next();
-					if (next.done) next = itr_temporary_rules.next();
-					// 繰り返し終了
-					if (next.done) return next;
-
-					// 無効化されているルールなら無視して次へ
-					if (next.value.is_disabled) {
-						continue;
-					}
-					break;
-				}
-				return next;
-			},
-			[Symbol.iterator]: () => {
-				return itr;
-			}
-		};
-		return itr;
+		return this._rules.getRulesItr(this._current_state);
 	}
 	addRule(label: string, rule: LexRule): void {
-		// 同名の既存ルールを破棄
-		this.removeRule(label);
-
-		const formatted_rule = LexController.formatLexRule(rule);
-		this._temporary_rules.rules.set(label, formatted_rule);
-		const states: Array<LexStateLabel> = formatted_rule.state !== undefined ? formatted_rule.state : [default_lex_state];
-		for (const state of states) {
-			if (this._temporary_rules.states.has(state)) {
-				this._temporary_rules.states.get(state)!.add(label);
-			}
-			else {
-				this._temporary_rules.states.set(state, new Set([label]));
-			}
-		}
+		this._rules.addRule(label, rule);
 	}
 	removeRule(label: string): LexRule | null {
-		if (!this._temporary_rules.rules.has(label)) {
-			return null;
-		}
-		const rule = this._temporary_rules.rules.get(label)!;
-		const states: Array<LexStateLabel> = rule.state !== undefined ? rule.state : [default_lex_state];
-		for (const state of states) {
-			if (this._temporary_rules.states.has(state)) {
-				this._temporary_rules.states.get(state)!.delete(label);
-			}
-		}
-		return rule;
+		return this._rules.removeRule(label);
 	}
 	getCurrentState(): LexStateLabel {
 		return this._current_state;
