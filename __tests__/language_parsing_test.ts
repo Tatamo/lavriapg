5e882b8102d2ab6dcc1d5502a2c740f39ca08d21
Tatamo <nsorder@gmail.com>
2018-03-26 16:02:23 +0900

    delete __tests__/data/language_language.ts

--- __tests__/language_parsing_test.ts
+++ __tests__/language_parsing_test.ts
@@ -1,10 +1,18 @@
 import {language_language, language_parser} from "../src/precompiler/ruleparser";
 import {ParserGenerator} from "../src/parsergenerator/parsergenerator";
-import {language_language_without_callback} from "./data/language_language";
 
 const input = require("fs").readFileSync("language", "utf8");
 
 describe("language parsing test", () => {
+	const lex = {...language_language.lex};
+	lex.rules = lex.rules.map((rule) => {
+		return {token: rule.token, pattern: rule.pattern};
+	});
+	const grammar = {...language_language.grammar};
+	grammar.rules = grammar.rules.map((rule) => {
+		return {ltoken: rule.ltoken, pattern: rule.pattern};
+	});
+	const language_language_without_callback = {lex, grammar};
 	const pg = new ParserGenerator(language_language);
 	console.error(pg.getTableType());
 	test("valid parser", () => {
